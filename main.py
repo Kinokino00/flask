7e89d3c4bdf25932389c1322e035b7c68e5ad1ec
kinolin <ichiki.ichiki@gmail.com>
2023-10-29 15:25:30 +0800

    完成PM2.5表格頁面

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 
 app = Flask(__name__)
 books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+ascending = True
 
 
 @app.route("/")  # app可隨意取名，/代表首頁
@@ -14,16 +15,33 @@ def index():
     return f"<h1>HELLO! {today}</h1>"
 
 
-@app.route("/pm25")
+@app.route("/pm25", methods=["GET", "POST"])
 def get_pm25():
+    global ascending
     url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=CSV"
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sort = False
+    # 確定回傳方式
+    if request.method == "POST":
+        # 判斷在html點的是否為排序鈕，要用request要記得import
+        print(request.form)
+        if request.form.get("sort"):
+            sort = True
+    print(sort)
     try:
         df = pd.read_csv(url).dropna()
+        # 升降序功能
+        if sort:
+            df = df.sort_values("pm25", ascending=ascending)
+            ascending = not ascending
+        else:
+            ascending = True
         columns = df.columns.tolist()
         values = df.values.tolist()
+        lowest = df.sort_values("pm25").iloc[0][["site", "pm25"]].values
+        highest = df.sort_values("pm25").iloc[-1][["site", "pm25"]].values
         message = "取得資料成功"
-        print(df)
+        print(df["pm25"].max())
     except Exception as e:
         print(e)
         message = "取得 PM 2.5 資料失敗，請稍後再試..."
